05c3ab6e4a59a35282ef3a99c208257c39dd25dc
Marc0570 <marc.hoekman@student.aventus.nl>
2024-06-06 14:44:10 +0200

    login pagina toevoegen

--- webshopdokado/routes/web.php
+++ webshopdokado/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    // return the view for the login page
+    return view('login');
+})->name('login');
