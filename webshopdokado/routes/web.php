142254eadae3588ebefdc528bfbbbe92ccc425f4
Marc0570 <marc.hoekman@student.aventus.nl>
2024-06-18 11:57:11 +0200

    Revert "Merge branch 'main' of https://github.com/MickAgyemang/ProjectWebshop"
    This reverts commit d2ad645af6ed41fc3e6484d2a5126440a11abaa7, reversing
    changes made to 764b91850f01a1e328a02f3b8b1049e9ad913ed2.

--- webshopdokado/routes/web.php
+++ webshopdokado/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,9 +13,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profiel routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route voor het opslaan van reviews
+    Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
 });
 
 require __DIR__.'/auth.php';
+
